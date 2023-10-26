99896b816c4bdb9d5c8f9d50725bffa0bf462324
GabrielNilsson <Gabriel.Nilsson@elev.ga.lbs.se>
2023-10-26 14:34:37 +0200

    Pathtracking

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -18,5 +18,22 @@ public class Enemy : MonoBehaviour
         //moves the enemy towards waypoints
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if(wavepointIndex >= Waypoints.wayPointsPath.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.wayPointsPath[wavepointIndex];
+
     }
 }
